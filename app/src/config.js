1ccfdab582223ae87e065841ea8ab6485305dd8a
Cody Thompson <cody.thompson22@gmail.com>
2018-08-24 22:45:58 -0700

    create-config - added defaults object - adds all fields passed in and all defaults not passed in as fields

---
+++ app/src/config.js
@@ -0,0 +1,34 @@
+const extend = require('lodash/extend')
+const defaults = require('lodash/defaults')
+
+/*
+ * default config values
+ */
+const defaultValues = {}
+
+class Config {
+  /**
+   * @constructor
+   * @param {object} localConfig the config values for this specific instance/machine
+   * @param {object} localConfig.dbUsername required - the mongodb username 
+   * @param {object} localConfig.dbPassword required - the mongodb password 
+   * @param {object} [localConfig.port=3000] the port to run on
+   */
+  constructor (localConfig) {
+    this._localConfig = localConfig;
+    let defaulted = defaults({...localConfig}, defaultValues)
+    extend(this, defaulted);
+  }
+
+  /**
+   * @property defaults
+   * @static
+   * @readonly
+   * the default values of the config object
+   */
+  static get defaults () {
+    return defaultValues
+  }
+}
+
+module.exports = Config
---
+++ app/src/config.test.js
@@ -0,0 +1,27 @@
+const Config = require('./config')
+
+const testConfigArgsA = {
+  dbUsername: 'blah',
+  dbPassword: 'some',
+  rando: 42
+}
+const testConfigArgsB = {
+  rando: '22' 
+}
+
+describe('config', () => {
+  const cA = new Config(testConfigArgsA)
+  const cB = new Config(testConfigArgsB)
+  it('should have a defaults object', () => {
+    expect(Config.defaults).toBeInstanceOf(Object);
+  })
+  it('should read some values from a passed in object', ()=> {
+    expect(cA.dbUsername).toStrictEqual('blah')
+    expect(cA.dbPassword).toStrictEqual('some')
+    expect(cA.rando).toStrictEqual(42)
+    expect(cB.dbUsername).not.toBeDefined();
+    expect(cB.dbPassword).not.toBeDefined()
+    expect(cB.rando).not.toStrictEqual(42)
+    expect(cB.rando).toStrictEqual('22')
+  })
+})
