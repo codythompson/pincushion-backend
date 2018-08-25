74f426185726df94d734e5c2c43fb4dabf84773c
Cody Thompson <cody.thompson22@gmail.com>
2018-08-24 19:02:31 -0700

    added app user to setup script

--- setup/dbUserSetup.js
+++ setup/dbUserSetup.js
@@ -1,8 +1,23 @@
 db = db.getSiblingDB('pincushion');
 
+db.dropUser('userAdmin');
 db.dropUser('schemaAdmin');
+db.dropUser('app');
+
+db.createUser({
+  user: 'userAdmin',
+  pwd: users.userAdmin,
+  roles: ['userAdmin']
+});
+
 db.createUser({
   user: 'schemaAdmin',
   pwd: users.setup,
   roles: ['dbAdmin']
+});
+
+db.createUser({
+  user: 'app',
+  pwd: users.app,
+  roles: ['readWrite']
 });
