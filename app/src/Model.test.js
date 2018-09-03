de86d5ca8806c1e1bbc0ea11a635c29d2823d8ad
Cody Thompson <cody.thompson22@gmail.com>
2018-09-02 22:59:51 -0700

    model-skeleton  - added connect static method  - added mongo mock skeleton

--- app/src/Model.test.js
+++ app/src/Model.test.js
@@ -1,21 +1,23 @@
+jest.mock('mongodb')
+
+const MongoClient = require('mongodb').MongoClient
+
 const Model = require('./Model')
 
-let mongodb, dbClient
-
 describe('Model', ()=>{
-  beforeAll(()=>{
-    // jest.mock('mongodb')
-    mongodb = require('mongodb')
-    return mongodb.MongoClient
-      .connect('mongodb://app:pwordreplacem@localhost/pincushion')
-      .catch(console.log.bind(console, 'mmk\n',))
-      .then((client) => {
-        dbClient = client
-      })
-  })
 
-  it('should accept a mongodb client instance as a constructor arg', () => {
-    const m = new Model(dbClient)
-    expect(m.client).toBeInstanceOf(mongodb.MongoClient)
+  it('should have a static connect method that takes a config object and returns a model instance', () => {
+    return Model.connect({
+      dbUsername: 'blah',
+      dbPassword: 'blah'
+    })
+    .then(m => {
+      try {
+        expect(m).toBeInstanceOf(Model)
+        expect(m.client).toBeInstanceOf(MongoClient)
+      } catch (e) {
+        return Promise.reject(e)
+      }
+    })
   })
 })
