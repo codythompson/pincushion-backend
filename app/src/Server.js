be1a2ea55bee6d74310d5b62bb7cb00c631266c3
Cody Thompson <cody.thompson22@gmail.com>
2018-10-27 10:47:55 -0700

    #14 - server-stop (#38)
    - added a stop method to the server
    - made it easier to use the start method in index outside of terminal

--- app/src/Server.js
+++ app/src/Server.js
@@ -2,10 +2,15 @@ class Server {
   constructor (app, config) {
     this.app = app
     this.config = config
+    this._httpServer = null
   }
 
   start () {
-    this.app.listen(this.config.port)
+    this._httpServer = this.app.listen(this.config.port)
+  }
+
+  stop () {
+    this._httpServer.close()
   }
 }
 
