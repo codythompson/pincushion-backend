da8f07b90c8e5eb5cb8990262e15365907033e62
Cody Thompson <cody.thompson22@gmail.com>
2018-10-27 10:37:36 -0700

    #8 - server-stop
    - added a stop method to the server
    - made it easier to use the start method in index outside of terminal

--- app/src/index.js
+++ app/src/index.js
@@ -4,18 +4,20 @@ const path = require('path')
 const Config = require('./Config')
 const Server = require('./Server')
 
-const start = (localConfigPath) => {
+const DEFAULT_PATH = path.resolve(__dirname, '../localConfig.json')
+
+const start = (localConfigPath=DEFAULT_PATH) => {
   const localConfig = require(localConfigPath)
   const config = new Config(localConfig)
   const app = express()
   const server = new Server(app, config)
   server.start();
+  return server;
 };
 
 // from https://stackoverflow.com/a/6090287
 if (require.main === module) {
-  const localConfigPath = path.resolve(__dirname, '../localConfig.json')
-  start(localConfigPath)
+  start()
 }
 
 module.exports = start
